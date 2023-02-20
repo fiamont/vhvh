62e771cdba3f435a505519a04448a2b3bf61366c
Sofia <sofia.montgomery@gmail.com>
2023-02-20 16:44:46 +0100

    first commit

---
+++ src/main/java/se/sofiatherese/vhvh/VhvhApplication.java
@@ -0,0 +1,13 @@
+package se.sofiatherese.vhvh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VhvhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VhvhApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/sofiatherese/vhvh/VhvhApplicationTests.java
@@ -0,0 +1,13 @@
+package se.sofiatherese.vhvh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VhvhApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
