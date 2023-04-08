74dce166b37e9cea7e500788af9bec781e9d8908
Sofia <sofia.montgomery@gmail.com>
2023-04-08 18:33:05 +0200

    added createsection method in service and controller

--- src/main/java/se/sofiatherese/vhvh/section/SectionController.java
+++ src/main/java/se/sofiatherese/vhvh/section/SectionController.java
@@ -1,4 +1,40 @@
 package se.sofiatherese.vhvh.section;
 
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
 public class SectionController {
+
+    private final SectionService sectionService;
+
+    @Autowired
+    public SectionController(SectionService sectionService) {
+        this.sectionService = sectionService;
+    }
+
+    @PostMapping("/createSection")
+    public ResponseEntity<SectionModel> createSection (@Valid @RequestBody SectionModel sectionModel, final PlaceModel placeModel, BindingResult result) {
+        return sectionService.makeSection(sectionModel, placeModel, result);
+    }
+
+
+    @GetMapping("/showallsections")
+
+
+    @GetMapping("/showallsectionsbyname")
+
+
+    @GetMapping("/showonesection")
+
+    @DeleteMapping("/deletesection/{sectionid}")
+
+
+    @PutMapping("/updatesection/{sectionid}")
+
+
 }
--- src/main/java/se/sofiatherese/vhvh/section/SectionService.java
+++ src/main/java/se/sofiatherese/vhvh/section/SectionService.java
@@ -1,4 +1,57 @@
 package se.sofiatherese.vhvh.section;
 
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class SectionService {
+
+    private final SectionRepository sectionRepository;
+
+    @Autowired
+    public SectionService(SectionRepository sectionRepository) {
+        this.sectionRepository = sectionRepository;
+    }
+
+    public ResponseEntity<SectionModel> makeSection (@Valid @RequestBody SectionModel sectionModel, final PlaceModel placeModel, BindingResult result){
+        try {
+            if (result.hasErrors()) {
+                return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+            sectionModel.setPlaceModel(placeModel);
+            sectionRepository.save(sectionModel);
+            return new ResponseEntity<>(sectionModel, HttpStatus.CREATED)
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public ResponseEntity<List<SectionModel>> viewAllSections (){
+
+    }
+
+    public ResponseEntity<List<SectionModel>> viewAllSectionsByName () {
+
+    }
+
+    public ResponseEntity<Optional<SectionModel>> viewOneSection () {
+
+    }
+
+    public ResponseEntity<SectionModel> removeSection () {
+
+    }
+
+    public ResponseEntity<SectionModel> changeSection () {
+
+    }
+
 }
