f1d8ac3a84b0e716763b594629e439a7ad659572
Sofia <sofia.montgomery@gmail.com>
2023-04-08 17:57:12 +0200

    added sectionrepository

--- src/main/java/se/sofiatherese/vhvh/section/SectionRepository.java
+++ src/main/java/se/sofiatherese/vhvh/section/SectionRepository.java
@@ -1,4 +1,14 @@
 package se.sofiatherese.vhvh.section;
 
-public interface SectionRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface SectionRepository extends JpaRepository<SectionModel, Long> {
+    //SectionModel findBySectionName (String sectionName);
+    @Query("SELECT s FROM SectionModel s ORDER BY s.sectionName asc")
+    List<SectionModel> orderBySectionName ();
 }
