b7b5d7f10d3748f50bf740e00afe8d4383124f72
Sofia <sofia.montgomery@gmail.com>
2023-04-08 17:43:44 +0200

    added sectionmodel

--- src/main/java/se/sofiatherese/vhvh/section/SectionModel.java
+++ src/main/java/se/sofiatherese/vhvh/section/SectionModel.java
@@ -1,4 +1,29 @@
 package se.sofiatherese.vhvh.section;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "section")
 public class SectionModel {
+
+    @SequenceGenerator(name = "sectionIdGenerator", allocationSize = 1)
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long sectionId;
+    @NotEmpty
+    private String sectionName;
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "place_id", nullable = false)
+    private PlaceModel placeModel;
+
+    public SectionModel(String sectionName) {
+        this.sectionName = sectionName;
+    }
 }
