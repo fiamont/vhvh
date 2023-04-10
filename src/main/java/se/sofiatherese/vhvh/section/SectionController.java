e7133c5d99cbe16ac9a75cf8ec4a77c9f0b15785
Sofia <sofia.montgomery@gmail.com>
2023-04-10 16:49:30 +0200

    added get, delete and putmappings

--- src/main/java/se/sofiatherese/vhvh/section/SectionController.java
+++ src/main/java/se/sofiatherese/vhvh/section/SectionController.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Optional;
+
 @Controller
 public class SectionController {
 
@@ -22,19 +25,29 @@ public class SectionController {
         return sectionService.makeSection(sectionModel, placeModel, result);
     }
 
-
     @GetMapping("/showallsections")
-
+    public ResponseEntity<List<SectionModel>> showAllSections () {
+        return sectionService.viewAllSections();
+    }
 
     @GetMapping("/showallsectionsbyname")
+    public ResponseEntity<List<SectionModel>> showAllSectionsByName () {
+        return sectionService.viewAllSectionsByName();
+    }
 
-
-    @GetMapping("/showonesection")
+    @GetMapping("/showonesection/{sectionid}")
+    public ResponseEntity<Optional<SectionModel>> showOneSection (@PathVariable Long sectionid) {
+        return sectionService.viewOneSection(sectionid);
+    }
 
     @DeleteMapping("/deletesection/{sectionid}")
-
+    public ResponseEntity<SectionModel> deleteSection (Long sectionId) {
+        return sectionService.removeSection(sectionId);
+    }
 
     @PutMapping("/updatesection/{sectionid}")
-
+    public ResponseEntity<SectionModel> updateSection (@PathVariable Long sectionid, @Valid @RequestBody SectionModel sectionModel, BindingResult result) {
+        return sectionService.changeSection(sectionid, sectionModel, result);
+    }
 
 }
--- src/main/java/se/sofiatherese/vhvh/section/SectionService.java
+++ src/main/java/se/sofiatherese/vhvh/section/SectionService.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
@@ -35,23 +36,53 @@ public class SectionService {
     }
 
     public ResponseEntity<List<SectionModel>> viewAllSections (){
-
+        try {
+            return ResponseEntity.ok(this.sectionRepository.findAll());
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     public ResponseEntity<List<SectionModel>> viewAllSectionsByName () {
+        try{
+            return ResponseEntity.ok(this.sectionRepository.orderBySectionName());
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public ResponseEntity<Optional<SectionModel>> viewOneSection (Long sectionId) {
+        try {
+            return new ResponseEntity<>(this.sectionRepository.findById(sectionId), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
 
     }
 
-    public ResponseEntity<Optional<SectionModel>> viewOneSection () {
-
+    public ResponseEntity<SectionModel> removeSection (Long sectionId) {
+        try {
+            sectionRepository.deleteById(sectionId);
+            return new ResponseEntity<>(null, HttpStatus.ACCEPTED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
-    public ResponseEntity<SectionModel> removeSection () {
+    public ResponseEntity<SectionModel> changeSection (@PathVariable Long sectionId, @Valid @RequestBody SectionModel sectionModel, BindingResult result) {
+        try {
+            if (result.hasErrors()) {
+                return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+            Optional<SectionModel> usedSection = sectionRepository.findById(sectionId);
+            SectionModel updatedSection = usedSection.get();
 
-    }
+            sectionRepository.save(updatedSection);
+            return new ResponseEntity<>(updatedSection, HttpStatus.OK);
 
-    public ResponseEntity<SectionModel> changeSection () {
-
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
 }
