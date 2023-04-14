7fcfd9bc87184fec243a920fcd00bede15443aa1
Sofia <sofia.montgomery@gmail.com>
2023-04-14 20:44:04 +0200

    added cascade

--- src/main/java/se/sofiatherese/vhvh/place/PlaceModel.java
+++ src/main/java/se/sofiatherese/vhvh/place/PlaceModel.java
@@ -19,7 +19,7 @@ public class PlaceModel {
     private Long placeId;
     @NotEmpty
     private String placeName;
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @ManyToOne(fetch = FetchType.LAZY, optional = false, cascade = CascadeType.ALL)
     @JoinColumn(name = "user_id", nullable = false)
     private UserModel userModel;
 
