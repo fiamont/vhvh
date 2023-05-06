8fc73d864c29be4dbfd28d6aa9501c6539b8c8b3
Sofia <sofia.montgomery@gmail.com>
2023-05-06 16:25:09 +0200

    added login endpoint and crossorigin annotations

--- src/main/java/se/sofiatherese/vhvh/auth/AuthController.java
+++ src/main/java/se/sofiatherese/vhvh/auth/AuthController.java
@@ -2,20 +2,29 @@ package se.sofiatherese.vhvh.auth;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
 @RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
 public class AuthController {
 
     private final AuthService authService;
 
+    @CrossOrigin
     @PostMapping("/authenticate")
     public ResponseEntity<AuthResponse> authenticate(@RequestBody AuthRequest request) {
+        return ResponseEntity.ok(authService.authenticate(request));
+    }
+
+    @CrossOrigin
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@RequestBody AuthRequest request) {
         return ResponseEntity.ok(authService.authenticate(request));
     }
 }
--- src/main/java/se/sofiatherese/vhvh/user/UserController.java
+++ src/main/java/se/sofiatherese/vhvh/user/UserController.java
@@ -1,7 +1,6 @@
 package se.sofiatherese.vhvh.user;
 
 import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
@@ -16,61 +15,61 @@ import java.util.Optional;
 public class UserController {
     private final UserService userService;
 
-    @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
     }
 
-
-    /*TODO:
-        7. Göra så inte all info kommer med så fort man hämtar en användare? DAO/DTO?
-            t.ex. bara username, role, firstname och lastname.
-        8. Se över put och matchmetoderna i service! "'Optional.get()' without 'isPresent()' check"
-           och "Method invocation 'setAccessible' may produce 'NullPointerException'"
-     */
-
+    @CrossOrigin
     @PostMapping("/registeruser")
     public ResponseEntity<UserModel> registerUser(@Valid @RequestBody UserModel userModel, BindingResult result) {
         return userService.createUser(userModel, result);
     }
 
+    @CrossOrigin
     @GetMapping("/showusers")
     public ResponseEntity<List<UserModel>> showUsers () {
         return userService.viewAllUsersAllInfo();
     }
 
+    @CrossOrigin
     @GetMapping("/showuser/{userid}")
     public ResponseEntity<Optional<UserModel>> showUser (@PathVariable Long userid) {
         return userService.getOneUser(userid);
     }
 
+    @CrossOrigin
     @GetMapping("/showusersbyfirstname")
     public ResponseEntity<List<UserModel>> showUsersByFirstname() {
         return userService.sortAllUsersByFirstname();
     }
 
+    @CrossOrigin
     @GetMapping("/showusersbylastname")
     public ResponseEntity<List<UserModel>> showUsersByLastname(){
         return userService.sortAllUsersByLastname();
     }
 
+    @CrossOrigin
     @GetMapping("/saveuseranna")
     public ResponseEntity<UserModel> registerUserAnna (){
 
         return userService.createUserAnna();
     }
 
+    @CrossOrigin
     @GetMapping("/saveuserbritta")
     public ResponseEntity<UserModel> registerUserBritta (){
 
         return userService.createUserBritta();
     }
 
+    @CrossOrigin
     @DeleteMapping("/deleteuser/{userid}")
     public ResponseEntity<UserModel> deleteUser (@PathVariable Long userid) {
         return userService.removeUser(userid);
     }
 
+    @CrossOrigin
     @PutMapping("/updateuser/{userid}")
     public ResponseEntity<UserModel> updateUser (@PathVariable Long userid, @RequestBody final UserModel userModel) {
         return userService.updateUser(userid, userModel);
