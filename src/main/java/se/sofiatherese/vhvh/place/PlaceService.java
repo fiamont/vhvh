f7f75ffb3cf93f4ed2a2c85203915642f3825a4f
Sofia <sofia.montgomery@gmail.com>
2023-05-26 16:47:09 +0200

    added isPresent in updatePlace, removed unused method in PlaceRepository

--- src/main/java/se/sofiatherese/vhvh/place/PlaceService.java
+++ src/main/java/se/sofiatherese/vhvh/place/PlaceService.java
@@ -10,15 +10,9 @@ import java.util.List;
 public interface PlaceService {
 
     ResponseEntity<PlaceModelDTO> createPlace (PlaceModelDTO placeModelDTO, BindingResult result, String username, PlaceModel placeModel);
-
     ResponseEntity<List<PlaceModelDTO>> viewAllPlaces (String username);
-
     ResponseEntity<List<PlaceModelDTO>> viewAllPlacesByName (String username);
-
     ResponseEntity<PlaceModelDTO> getOnePlace(Long placeId);
-
     ResponseEntity<PlaceModel> removePlace(Long placeId);
-
     ResponseEntity<PlaceModel> updatePlace(Long placeId, PlaceModel placeModel, BindingResult result);
-
 }
--- src/main/java/se/sofiatherese/vhvh/place/PlaceServiceImpl.java
+++ src/main/java/se/sofiatherese/vhvh/place/PlaceServiceImpl.java
@@ -25,22 +25,18 @@ public class PlaceServiceImpl implements PlaceService{
             if (result.hasErrors()) {
                 return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
             }
-
             if (placeRepository.existsByPlaceNameAndUserModel_Username(placeModelDTO.getPlaceName(), username)) {
                 return new ResponseEntity<>(null, HttpStatus.CONFLICT);
             }
-
             PlaceModel placeModel = PlaceModel.builder().placeName(placeModelDTO.getPlaceName()).build();
             UserModel userModel = userRepository.findByUsername(username).orElseThrow();
             placeModel.setUserModel(userModel);
             placeRepository.save(placeModel);
-
             PlaceModelDTO createdPlaceModelDTO = PlaceModelDTO.builder()
                     .placeId(placeModel.getPlaceId())
                     .placeName(placeModel.getPlaceName())
                     .userId(placeModel.getUserModel().getUserId())
                     .build();
-
             return new ResponseEntity<>(createdPlaceModelDTO, HttpStatus.CREATED);
         } catch (Exception e) {
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
@@ -115,11 +111,13 @@ public class PlaceServiceImpl implements PlaceService{
         }
         try {
             Optional<PlaceModel> usedPlace = placeRepository.findById(placeId);
-            PlaceModel updatedPlace = usedPlace.get();
-
-            updatedPlace.setPlaceName(placeModel.getPlaceName());
-            placeRepository.save(updatedPlace);
-            return new ResponseEntity<>(updatedPlace, HttpStatus.OK);
+            if(usedPlace.isPresent()){
+                PlaceModel updatedPlace = usedPlace.get();
+                updatedPlace.setPlaceName(placeModel.getPlaceName());
+                placeRepository.save(updatedPlace);
+                return new ResponseEntity<>(updatedPlace, HttpStatus.OK);
+            }
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
         } catch (Exception e) {
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
